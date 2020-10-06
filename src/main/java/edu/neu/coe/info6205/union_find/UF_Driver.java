3ff877aebeb639e196db1743d7662d3c3bc34eac
Yin Song <song.yin1@northeastern.edu>
2020-10-05 23:51:16 -0400

    analysis

---
+++ src/main/java/edu/neu/coe/info6205/union_find/UF_Driver.java
@@ -0,0 +1,47 @@
+package edu.neu.coe.info6205.union_find;
+
+import java.util.Random;
+
+public class UF_Driver {
+	
+	public static int uf_helper(int n) {
+		UF_HWQUPC u = new UF_HWQUPC(n, false);
+		
+		Random random = new Random();
+		int c = 0;
+		while (u.components() != 1) {
+			int n1 = random.nextInt(n);
+			int n2 = random.nextInt(n);
+			if (!u.connected(n1, n2)) {
+				u.union(n1, n2);
+			}
+			c++;
+		}
+		return c;
+	}
+
+	public static void main(String[] args) {
+		int runs = 100;
+		double c = 0; //coefficient
+		int count = 0;
+
+		for (int i = 100; i < 30000; i += 100) {
+
+			int total = 0;
+			for (int jk = 0; jk < runs; jk++) {
+				total = total + uf_helper(i);
+			}
+
+			count++;
+			double logFactor = Math.log(i) * i;
+			c += (total / runs) / logFactor;
+			System.out.println("n = " + i + ", pairs = " + total / runs + ", run = " + runs);
+			System.out.println("n = " + i + ", m = " + total / runs + ", c = " + ((total / runs) / logFactor) + "\n");
+		}
+
+		System.out.println("Avg(c) = " + (c / count));
+
+	}
+
+	
+}
